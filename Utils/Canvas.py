39c05efd4ce6b05e14d5e1c873c08d101a3b6ec8
Kevin Martinez <ksmartinez2307@gmail.com>
2022-07-23 15:32:45 -0500

    Applied styles to graph img

--- Utils/Canvas.py
+++ Utils/Canvas.py
@@ -5,24 +5,24 @@ from matplotlib.backends.backend_qtagg import FigureCanvas
 
 class Canvas(FigureCanvas):
 
-    def __init__(self, plot, is_directed, parent=None):
+    def __init__(self, plot, is_directed):
         self.fig, self.ax = plt.subplots(1, figsize=(10, 10))
         super().__init__(self.fig)
 
         pos = nx.spring_layout(plot, seed=len(plot), k=10)
         edges_weight = nx.get_edge_attributes(plot, 'weight')
 
-        nx.draw_networkx_nodes(plot, pos)
-        nx.draw_networkx_labels(plot, pos)
+        nx.draw_networkx_nodes(plot, pos, node_color='#fe79c7')
+        nx.draw_networkx_labels(plot, pos, font_color='#383a59', font_size=10)
 
         if is_directed:
-            nx.draw_networkx_edges(plot, pos, arrows=True)
+            nx.draw_networkx_edges(plot, pos, arrows=True, edge_color='#6272a4')
         else:
-            nx.draw_networkx_edges(plot, pos, arrows=False)
+            nx.draw_networkx_edges(plot, pos, arrows=False, edge_color='#6272a4')
 
-        nx.draw_networkx_edge_labels(plot, pos, edge_labels=edges_weight)
+        nx.draw_networkx_edge_labels(plot, pos, edge_labels=edges_weight, font_color='#383a59')
 
-        # print(nx.dijkstra_path(plot, source='D', target='C'))
-
+        self.ax.set_facecolor('#383a59')
         self.ax.axis('off')
+        self.fig.set_facecolor('#383a59')
         self.ax.plot()
